d8449f3ee4b1167ae583dc90de83a6ecdaedaa5a
6asd <62227711+6asd@users.noreply.github.com>
2022-09-17 10:38:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="www.211985.xyz";
+        url.hostname="https://211985.xyz";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
